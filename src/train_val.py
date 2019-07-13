5fa553a32a3aa184a8e5f3adde82d39198c62d61
Anton Ponomarchuk <ponomarchuk.anton@gmail.com>
2019-07-14 01:38:17 +0300

    split code into functions

--- src/train_val.py
+++ src/train_val.py
@@ -16,22 +16,18 @@ def train(model, data_loader, epoch, optimizer, criterion, metric, board_writer=
 
         optimizer.zero_grad()
         output_masks = model(img_batch)
-        loss = 0.
-        for i in range(output_masks.shape[1]):
-            loss += criterion(output_masks[:, i].reshape(-1, 1), masks_batch[:, i].reshape(-1, 1))
-            results = metric( output_masks[:, i].reshape(-1, 1), masks_batch[:, i].reshape(-1 , 1), threshold)
-            #print('results: ', results)
-            train_true_positive += results[0]
-            train_false_positive += results[1]
-            train_false_negative += results[2]
+        loss, true_positive, false_positive, false_negative = current_loss_metric_calc(criterion, metric, output_masks,
+                                                                                       masks_batch, threshold)
 
         loss.backward()
         optimizer.step()
         train_loss += loss.item()
-    #print(train_true_positive, train_false_positive, train_false_negative)
-    precision = train_true_positive / (train_true_positive + train_false_positive)
-    recall = train_true_positive / (train_true_positive + train_false_negative)
-    f1_score = 2 * (precision * recall / (precision + recall))
+        train_true_positive += true_positive
+        train_false_positive += false_positive
+        train_false_negative += false_negative
+
+    print(train_true_positive, train_false_positive, train_false_negative)
+    precision, recall, f1_score = metric_calculation(train_true_positive, train_false_positive, train_false_negative)
     if board_writer is not None:
         scalars_dict['train/loss'] = train_loss / data_len
         scalars_dict['train/precision'] = precision
@@ -56,21 +52,14 @@ def val(model, criterion, metric, data_loader, epoch, board_writer, device='cpu'
         for idx, input_batch in pbar:
             img_batch = input_batch['image'].to(device)
             masks_batch = input_batch['labels'].to(device)
-
             output_masks = model(img_batch)
-
-            loss = 0
-            for i in range(output_masks.shape[1]):
-                loss += criterion( output_masks[:, i].reshape(-1, 1), masks_batch[:, i].reshape(-1, 1))
-                results = metric(output_masks[:, i].reshape(-1, 1), masks_batch[:, i].reshape(-1 , 1), threshold)
-                val_true_positive += results[0]
-                val_false_positive += results[1]
-                val_false_negative += results[2]
-
+            loss, true_positive, false_positive, false_negative = current_loss_metric_calc(criterion, metric, output_masks,
+                                                                                           masks_batch, threshold)
             val_loss += loss.item()
-        precision = val_true_positive / (val_true_positive + val_false_positive)
-        recall = val_true_positive / (val_true_positive + val_false_negative)
-        f1_score = 2 * (precision * recall / (precision + recall))
+            val_true_positive += true_positive
+            val_false_positive += false_positive
+            val_false_negative += false_negative
+        precision, recall, f1_score = metric_calculation(val_true_positive, val_false_positive, val_false_negative)
         if board_writer is not None:
             scalars_dict['val/loss'] = val_loss / data_len
             scalars_dict['val/precision'] = precision
@@ -86,3 +75,24 @@ def val(model, criterion, metric, data_loader, epoch, board_writer, device='cpu'
 def log_scalars(board_writer, scalars_dict, epoch):
     for key in scalars_dict.keys():
         board_writer.add_scalar(key, scalars_dict[key], epoch)
+
+
+def current_loss_metric_calc(criterion, metric, output_masks, masks_batch, threshold):
+    loss = 0
+    true_positive = 0
+    false_positive = 0
+    false_negative = 0
+    for i in range(output_masks.shape[1]):
+        loss += criterion(output_masks[:, i].reshape(-1, 1), masks_batch[:, i].reshape(-1, 1))
+        results = metric(output_masks[:, i].reshape(-1, 1), masks_batch[:, i].reshape(-1, 1), threshold)
+        true_positive += results[0]
+        false_positive += results[1]
+        false_negative += results[2]
+    return loss, true_positive, false_positive, false_negative
+
+
+def metric_calculation(true_positive, false_positive, false_negative):
+    precision = true_positive / (true_positive + false_positive)
+    recall = true_positive / (true_positive + false_negative)
+    f1_score = 2 * (precision * recall / (precision + recall))
+    return precision, recall, f1_score
